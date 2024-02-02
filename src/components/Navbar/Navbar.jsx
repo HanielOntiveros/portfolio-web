84c5c54b1d58a5fc2c7dec6e798773523d0be557
HanielOntiveros <haniel97@hotmail.com>
2024-02-02 11:57:48 -0600

    desktop navbar version done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,11 +11,14 @@ export const Navbar = () => {
     }
 
     return (
-        <nav className='fixed w-full top-0 end-0 h-16 z-10 md:h-24'>
+        <nav className='fixed w-full top-0 end-0 h-16 z-10 md:h-24 '>
             <div className="flex h-full bg-black">
                 <div className='flex flex-col '>
+                    <div className='hidden lg:flex'>
+                        <NavbarMenu />
+                    </div>
                     <button
-                        className='h-full w-full p-2 z-20 lg:hidden'
+                        className='h-full w-full p-2 z-20 lg:hidden '
                         onClick={handleMenu}
                     >
                         {
@@ -35,11 +38,11 @@ export const Navbar = () => {
                         }
                     </button>
                 </div>
-                <div className='absolute flex  w-full h-full justify-center '>
+                <div className='absolute flex  w-full h-full justify-center  lg:justify-start lg:m-3'>
                     <h1 className='text-white '>sH dev<br />logo</h1>
                 </div>
             </div>
-            <hr className=" decoration-white border-1 md:border-2 lg:w-1/2 lg:" />
+            <hr className=" decoration-white border-1 md:border-2 lg:w-1/2 lg:absolute lg:end-0" />
             {toggleMenu ? <NavbarMenu /> : null}
         </nav >
     )
--- src/components/Navbar/NavbarMenu.jsx
+++ src/components/Navbar/NavbarMenu.jsx
@@ -1,15 +1,15 @@
 
 export const NavbarMenu = () => {
     return (
-        <div className="relative  h-auto w-screen flex justify-center z-20">
-            <div className="w-screen h-auto  bg-black ">
-                <ul className="w-full h-full flex flex-col items-center">
-                    <li className='inline-block my-3  w-full text-2xl font-regular leading-loose text-white text-center hover:font-bold hover:cursor-pointer md:text-5xl md:my-7'>ABOUT</li>
-                    <hr className="w-2/3" />
-                    <li className='inline-block my-3 w-full text-2xl font-regular leading-loose text-white text-center hover:font-bold hover:cursor-pointer md:text-5xl md:my-7'>PROJECTS</li>
-                    <hr className="w-2/3" />
-                    <li className='inline-block my-3 w-full text-2xl font-regular leading-loose text-white text-center hover:font-bold hover:cursor-pointer md:text-5xl md:my-7'>CONTACT</li>
-                    <hr className="w-2/3 mb-4" />
+        <div className="relative  h-auto w-screen flex justify-center z-20 ">
+            <div className="w-screen h-auto  bg-black  lg:w-1/2 lg:absolute lg:end-0 lg:mr-2">
+                <ul className="w-full h-full flex flex-col items-center lg:flex-row lg:justify-evenly lg:gap-10  lg:px-3">
+                    <li className='inline-block my-3  w-full text-2xl font-regular leading-loose text-white text-center hover:font-bold hover:cursor-pointer md:text-5xl md:my-7 lg:text-3xl lg:w-auto'>ABOUT</li>
+                    <hr className="w-2/3 lg:hidden" />
+                    <li className='inline-block my-3 w-full text-2xl font-regular leading-loose text-white text-center hover:font-bold hover:cursor-pointer md:text-5xl md:my-7 lg:text-3xl lg:w-auto'>PROJECTS</li>
+                    <hr className="w-2/3 lg:hidden" />
+                    <li className='inline-block my-3 w-full text-2xl font-regular leading-loose text-white text-center hover:font-bold hover:cursor-pointer md:text-5xl md:my-7 lg:text-3xl lg:w-auto'>CONTACT</li>
+                    <hr className="w-2/3 mb-4 lg:hidden" />
                 </ul>
 
             </div>
